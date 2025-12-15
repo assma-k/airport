66d6f2fac6a226673886e5ed7eedf7aa84e26127
sudo passwd assma <assma@LAPTOP-SR4FB68L.localdomain>
2025-12-15 16:54:21 +0100

    files

--- airport.php
+++ airport.php
@@ -1,17 +1,37 @@
 <?php
-function defilement($chaines) {
+function defilement($chaines, $time) {
+   echo "choix couleur :\n";
+   echo "1 : rouge\n";
+   echo "2 bleu\n";
+   $choix = trim(fgets(STDIN));
+
 
     $long = strlen($chaines);
-    for ($j = 0; $j <= 30 ; $j++) {
+    if($choix == 1){
+    for ($j = 0; $j <= $time ; $j++) {
 
         for ($i = 0; $i < $long; $i++) {
-            echo substr($chaines, $i) . substr($chaines, 0, $i) . " \n";
-            usleep(500000);
+            echo "\033[31m" . "\r" . substr($chaines, $i) . substr($chaines, 0, $i) ."\033[0m" ;
+            usleep(100000);
 
             };
-            popen('cls', 'w');
+            
         };
+    }
+     elseif($choix == 2) {
+        for ($j = 0; $j <= $time ; $j++) {
+
+        for ($i = 0; $i < $long; $i++) {
+            echo "\033[34m" . "\r" . substr($chaines, $i) . substr($chaines, 0, $i) ."\033[0m" ;
+            usleep(100000);
+
+            };
+            
+        };
+     } 
 }
 
+$time = $argv[2];
 $chaines = $argv[1];
-defilement($chaines);
+defilement($chaines, $time);
+
