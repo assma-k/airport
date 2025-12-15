452f3686e440ffd940666b71eb6330b0db3cc13a
sudo passwd assma <assma@LAPTOP-SR4FB68L.localdomain>
2025-12-15 16:02:49 +0100

    files

--- airport.php
+++ airport.php
@@ -1,13 +1,18 @@
 <?php
-function defilement($chaines){
-    $long = strlen($chaines);
-    for($j = 0; $j <= 30 ; $j++){
-    for($i = 0; $i < $long; $i++){
-        echo substr($chaines, $i) . substr($chaines, 0, $i) . " \n";
-        usleep(500000);
+function defilement($chaines) {
 
-    };
-};
+    $long = strlen($chaines);
+    for ($j = 0; $j <= 30 ; $j++) {
+
+        for ($i = 0; $i < $long; $i++) {
+            echo substr($chaines, $i) . substr($chaines, 0, $i) . " \n";
+            usleep(500000);
+
+            };
+            break;
+            popen('cls', 'w');
+        };
 }
-    $chaines = $argv[1];
-    defilement($chaines);
+
+$chaines = $argv[1];
+defilement($chaines);
